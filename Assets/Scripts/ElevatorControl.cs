c64d26a42c962e2ecde85b5a9c6972b7f62e8069
Avi Miller <TrebecS@Gmail.com>
2018-03-24 00:11:42 -0700

    Added lights and elevator

---
+++ Assets/Scripts/ElevatorControl.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ElevatorControl : MonoBehaviour {
+
+	[SerializeField] Elevator elevator;
+
+	bool playerInRegion = false;
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.E) && playerInRegion){
+			elevator.goToNextFloor();
+		}
+		if (Input.GetKeyDown(KeyCode.R) && playerInRegion){
+			elevator.goToPrevFloor();
+		}
+	}
+
+	void OnTriggerEnter(){
+		print("entered");
+		playerInRegion = true;
+	}
+
+	void OnTriggerExit(){
+		print("exited");
+		playerInRegion = false;
+	}
+}
---
+++ Assets/Scripts/RotatingLights.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotatingLights : MonoBehaviour {
+
+	[SerializeField] float speed = 6f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.rotation *= Quaternion.AngleAxis(speed * Time.deltaTime, Vector3.up);
+	}
+}
